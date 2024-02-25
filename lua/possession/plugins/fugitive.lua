ce57083f8a16f993433746ae2b03e7f29ef1ec94
Amit Tamari <15573913+amittamari@users.noreply.github.com>
2024-02-25 10:53:56 +0200

    fix fugitive

--- lua/possession/plugins/fugitive.lua
+++ lua/possession/plugins/fugitive.lua
@@ -1,11 +1,11 @@
-local create_plugin_reopener = require('possession.plugins.plugin_win_reopener').create_plugin_reopener
+local create_plugin_win_reopener = require('possession.plugins.plugin_win_reopener').create_plugin_win_reopener
 
 local has_fugitive = function()
     -- TODO has solution?
     return true
 end
 
-local M = create_plugin_reopener('fugitive', function()
+local M = create_plugin_win_reopener('fugitive', function()
     vim.cmd('G')
 end, has_fugitive)
 
